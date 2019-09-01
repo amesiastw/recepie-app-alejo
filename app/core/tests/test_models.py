2f4b9aa08a5a457ae69b72ce1f98211c259eda2a
alejandrom <amesias@thoughtworks.com>
2019-09-01 17:23:00 -0500

    Alejo: Adds CustomUser model with its tests

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        email = 'test@gmail.com'
+        password = 'testPass'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        email = 'test@GMAIL.com'
+        password = 'testPass'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email.lower())
